62d500b4b190a3aaf0b4d051f644e0f9951c6251
Слава Муравьёв <MuravjevSlava@gmail.com>
2018-12-22 16:26:10 +0400

    Z, Prefix and Trie contest: - added solution of A - added solution of B

---
+++ December_22_Z_Prefix_Trie/A.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+
+void read_ints(size_t size, vector<int> & a){
+    a.resize(size);
+    for (int & value : a) cin >> value;
+}
+
+void print_ints(vector<int> & a, char sep = ' ') {
+    for (int value : a) cout << value << sep;
+    cout << '\n';
+}
+
+template <class T>
+vector<int> compress(vector<T> values) {
+    vector<T> sorted = values; // copying
+    sort(sorted.begin(), sorted.end());
+    sorted.resize(unique(sorted.begin(), sorted.end()) - sorted.begin());
+
+    vector<int> result(values.size());
+    for (int i = 0; i < values.size(); ++i) {
+        result[i] = lower_bound(sorted.begin(), sorted.end(), values[i]) - sorted.begin();
+    }
+
+    return result;
+}
+
+vector<int> NOT_ANSWER(0);
+
+int get_bit(int mask, int bit) {
+    return (mask >> bit) & 1;
+}
+
+template <class T>
+vector<int> prefix_function(T & s) {
+    size_t n = s.size();
+    if (n == 0) return NOT_ANSWER;
+
+    vector<int> prefix(n);
+
+    prefix[0] = 0;
+    for (int i = 1; i < n; ++i) {
+        int j = prefix[i - 1];
+        while (j > 0 && s[i] != s[j]) {
+            j = prefix[j - 1];
+        }
+
+        if (s[i] == s[j]) {
+            ++j;
+        }
+
+        prefix[i] = j;
+    }
+
+    return prefix;
+}
+
+template <class T>
+vector<int> z_function(T & s) {
+    size_t n = s.size();
+    if (n == 0) return vector<int>(0);
+
+    vector<int> z(n);
+    z[0] = 0;
+
+    for (int i = 1, l = 0, r = 0; i < n; ++i) {
+        if (r < i) {
+            l = i;
+            r = i;
+        }
+
+        z[i] = min(z[i - l], r - i);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]]) ++z[i];
+
+        if (r < i + z[i]) {
+            l = i;
+            r = i + z[i];
+        }
+    }
+
+    return z;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    string s;
+    cin >> s;
+
+    vector<int> z = z_function(s);
+
+    z[0] = s.size();
+    print_ints(z);
+
+    return 0;
+}
